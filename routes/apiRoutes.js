6ff8aa36d74739a956ad00da201799ada9156572
Meline Sarkissian <sarkissian.mell321@yahoo.com>
2023-11-01 15:18:14 -0700

    made api routes and note routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,40 @@
-let notes = []; // Temporary storage. Replace this with persistent storage later.
 
-module.exports = function(app) {
-    // Fetch existing notes
-    app.get('/api/notes', (req, res) => {
-        res.json(notes);
-    });
+const path = require('path');
+const fs = require('fs')
 
-    // Save a new note
-    app.post('/api/notes', (req, res) => {
-        let newNote = req.body;
+// this npm package allows for unique ids to be created
+var uniqid = require('uniqid');
+
+
+module.exports = (app) => {
+
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+ 
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      
+      id: uniqid(),
+    };
+ 
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+
+  });
+
+  app.delete('/api/notes/:id', (req, res) => {
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
     
-        notes.push(newNote);
-        res.json(newNote);
-    });
-
-    // Delete a note by ID
-    app.delete('/api/notes/:id', (req, res) => {
-        let noteId = req.params.id;
-        let noteIndex = notes.findIndex(note => note.id === noteId);
-        if (noteIndex !== -1) {
-            notes.splice(noteIndex, 1);
-            res.json({ success: true, message: 'Note deleted' });
-        } else {
-            res.status(404).json({ success: false, message: 'Note not found' });
-        }
-    });
+  })
 };
